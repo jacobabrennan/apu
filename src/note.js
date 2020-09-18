81f28f31de1e511f9117325c4d004551aab1caa2
Jacob A Brennan <jacobabrennan@gmail.com>
2020-09-18 14:42:59 -0400

    Fix bug in note looping

--- src/note.js
+++ src/note.js
@@ -6,7 +6,7 @@
 import AudioProcessor from './audio_processor.js';
 
 //------------------------------------------------
-export default class Note extends AudioProcessor {
+class Note extends AudioProcessor {
     constructor(instrument, value) {
         super();
         this.value = value;
@@ -17,30 +17,34 @@ export default class Note extends AudioProcessor {
         this.volume = this.volumeGoal;
     }
     sample() {
+        // Generate silence beyond envelope
         if(this.nodeIndexCurrent >= this.instrument.envelopeVolume.length) {
             return 0;
-            // return null;
         }
-        //
+        // Generate constant tone on sustain node
         if(!this.live && this.nodeIndexCurrent === this.instrument.sustain) {
             return this.instrument.envelopeVolume[this.nodeIndexCurrent];
         }
+        // Advance node at end of node duration
         if(this.duration-- <= 0) {
             this.nodeIndexCurrent++;
+            // Loop nodes
+            if(!this.live && this.nodeIndexCurrent > this.instrument.loopEnd) {
+                this.nodeIndexCurrent = this.instrument.loopStart;
+            }
+            // Generate silence beyond envelope
             if(this.nodeIndexCurrent >= this.instrument.envelopeVolume.length) {
                 this.duration = Infinity;
                 return 0;
-                // return null;
             }
-            if(!this.live && this.nodeIndexCurrent === this.instrument.loopEnd) {
-                this.nodeIndexCurrent = this.instrument.loopStart;
-            }
+            //
             this.volume = this.volumeGoal;
             this.volumeGoal = this.instrument.envelopeVolume[this.nodeIndexCurrent];
             this.duration = this.instrument.envelopeDuration[this.nodeIndexCurrent];
         }
-        //
+        // Advance volume towards goal
         this.volume += (this.volumeGoal - this.volume) / Math.max(1, this.duration);
+        // Generate Sample
         return this.volume;
     }
     cut() {
