61acbf12031f31ba642715edc541e6222098285a
Jacob A Brennan <jacobabrennan@gmail.com>
2020-09-18 16:36:47 -0400

    Update apu.single.js with fixes in src/note.js

--- apu.single.js
+++ apu.single.js
@@ -208,36 +208,41 @@ class Note extends AudioProcessor {
         this.duration = 0;
         this.volumeGoal = instrument.envelopeVolume[0];
         this.volume = this.volumeGoal;
+        this.live = true;
     }
     sample() {
+        // Generate silence beyond envelope
         if(this.nodeIndexCurrent >= this.instrument.envelopeVolume.length) {
             return 0;
-            // return null;
         }
-        //
-        if(!this.live && this.nodeIndexCurrent === this.instrument.sustain) {
+        // Generate constant tone on sustain node
+        if(this.live && this.nodeIndexCurrent === this.instrument.sustain) {
             return this.instrument.envelopeVolume[this.nodeIndexCurrent];
         }
+        // Advance node at end of node duration
         if(this.duration-- <= 0) {
             this.nodeIndexCurrent++;
+            // Loop nodes
+            if(this.live && this.nodeIndexCurrent > this.instrument.loopEnd) {
+                this.nodeIndexCurrent = this.instrument.loopStart;
+            }
+            // Generate silence beyond envelope
             if(this.nodeIndexCurrent >= this.instrument.envelopeVolume.length) {
                 this.duration = Infinity;
                 return 0;
-                // return null;
             }
-            if(!this.live && this.nodeIndexCurrent === this.instrument.loopEnd) {
-                this.nodeIndexCurrent = this.instrument.loopStart;
-            }
+            //
             this.volume = this.volumeGoal;
             this.volumeGoal = this.instrument.envelopeVolume[this.nodeIndexCurrent];
             this.duration = this.instrument.envelopeDuration[this.nodeIndexCurrent];
         }
-        //
+        // Advance volume towards goal
         this.volume += (this.volumeGoal - this.volume) / Math.max(1, this.duration);
+        // Generate Sample
         return this.volume;
     }
     cut() {
-        this.live = true;
+        this.live = false;
     }
     retrigger() {
         this.nodeIndexCurrent = 0;
